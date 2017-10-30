a447c5e9d192a84004d42966ee85bc2ec9a721c8
Aaron Anderson <aaronlynanderson@gmail.com>
2017-10-29 23:05:20 -0500

    People.sol deploys to network. I had to add People.sol to 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var People = artifacts.require("./People.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(People);
 };
